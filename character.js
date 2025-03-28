9b96466afcafac9da78ba721173734f15b12883c
Brando00-uni <182504869+PaigeM1288@users.noreply.github.com>
2025-03-28 22:26:12 +0000

    character.js

---
+++ character.js
@@ -0,0 +1,17 @@
+class Character{
+    #x;
+    #y;
+    #xSpeed;
+    #ySpeed;
+    #colour;
+    #name;
+
+    constructor(x, y, colour, name){
+        this.#x = x;
+        this.#y = y;
+        this.#xSpeed = 0;
+        this.#ySpeed = 0;
+        this.#colour = colour;
+        this.#name = name;
+    }
+}
